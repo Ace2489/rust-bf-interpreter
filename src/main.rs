3c39e52d954bb16ed89a834aa3fed4b8252b3614
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2025-01-28 11:24:17 +0100

    interpreter built for all commands except brackets

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,61 @@
-struct Machine{
-    instruction_pointer:usize,
-    data: [u8;0x7530],
+#[derive(Debug)]
+struct Machine {
+    instruction_pointer: usize,
+    data: [u8; 10],
     data_pointer: usize,
-    input: String,
-    output: String
-
+    input: Vec<u8>,
+    output: Vec<u8>,
 }
 
-impl Machine{
-    fn run(&self, program: String){
-        let Some(instruction) = program.chars().nth(self.instruction_pointer) else {panic!("Something's wrong with the pointer for the instructions")};
-        if instruction == ';'{return}
-        match instruction{
-            '>' =>{println!("The right angle is here with the data at {:?}", self.input.chars().nth(self.data_pointer))},
-            _=>panic!("Something went wrong here")
-        };
+impl Machine {
+    fn run(&mut self, program: String) {
+        loop {
+            let Some(instruction) = program.chars().nth(self.instruction_pointer) else {
+                panic!("An error occured while reading the characters from the input")
+            };
+            if instruction == ';' {
+                return;
+            }
+
+            if instruction == '['{
+                
+            }
+            match instruction {
+                '>' => self.data_pointer += 1,
+                '<' => self.data_pointer -= 1,
+                '+' => self.data[self.data_pointer] += 1,
+                '-' => self.data[self.data_pointer] -= 1,
+                '.' => self.output.push(self.data[self.data_pointer]),
+                ',' => {
+                    let Some(element) = self.input.pop() else {
+                        panic!("There is no input to read from")
+                    };
+                    self.data[self.data_pointer] = element
+                }
+                _ => {}
+            };
+            self.advance_instruction_pointer();
+        }
+    }
+    fn advance_instruction_pointer(&mut self) {
+        self.instruction_pointer += 1;
     }
 }
 
 fn main() {
-    let program = "><+;".to_string();
-    let input = String::from("in");
-    let output = String::from("out");
+    let program = ">>+++-.,;".to_string();
+    let input: Vec<u8> = vec![1, 2, 3];
+    let output: Vec<u8> = Vec::new();
 
-    let machine = Machine{
+    let mut machine = Machine {
         instruction_pointer: 0 as usize,
-        data: [0;30000],
+        data: [0; 10],
         data_pointer: 0 as usize,
         input,
-        output
+        output,
     };
 
+    println!("{:#?}", machine);
     machine.run(program);
-
-
+    println!("{:#?}", machine)
 }
-
